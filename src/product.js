7464340b8e1c410d3fc396a6ff694eff2ab46029
nesaenelle <nesaenelle@gmail.com>
2019-05-12 13:46:00 +0300

    fix

---
+++ src/product.js
@@ -0,0 +1,16 @@
+(function($) {
+
+    $('.slider').slick({
+        // centerMode: true,
+        slidesToShow: 1,
+        // slidesToScroll: 1,
+        // dots: true,
+        arrows: true,
+        // infinite: true,
+        cssEase: 'linear',
+        draggable: false
+        // variableWidth: true,
+        // variableHeight: true
+    });
+
+})(jQuery);
