cc0220d71277931c6d5a86ef1bea3aa4940cfbe1
nesaenelle <nesaenelle@gmail.com>
2019-05-13 21:07:44 +0300

    fix

--- src/you-should-know.js
+++ src/you-should-know.js
@@ -9,44 +9,15 @@ $(document).ready(function () {
         slidesToShow: 2,
         // autoplay: true,
         responsive: [
-            // {
-            //     breakpoint: 1600,
-            //     settings: {
-            //         // centerMode: true,
-            //         slidesToShow: 1
-            //     }
-            // },
-            // {
-            //     breakpoint: 1280,
-            //     settings: {
-            //         // centerMode: true,
-            //         slidesToShow: 4
-            //     }
-            // },
-            // {
-            //     breakpoint: 1024,
-            //     settings: {
-            //         // centerMode: true,
-            //         slidesToShow: 3
-            //     }
-            // },
-            // {
-            //     breakpoint: 768,
-            //     settings: {
-            //         // centerMode: true,
-            //         slidesToShow: 2
-            //     }
-            // },
-            // {
-            //     breakpoint: 480,
-            //     settings: {
-            //         // arrows: false,
-            //         // dots: false,
-            //         // centerMode: true,
-            //         // centerPadding: '20px',
-            //         slidesToShow: 1
-            //     }
-            // }
+
+            {
+                breakpoint: 1024,
+                settings: {
+                    // centerMode: true,
+                    slidesToShow: 1
+                }
+            }
+            
         ]
     });
 
