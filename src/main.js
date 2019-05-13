73284e67bd70b5eb968b284ee57921c29442d07e
nesaenelle <nesaenelle@gmail.com>
2019-05-13 23:04:01 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -13,35 +13,7 @@ $(document).ready(function () {
     });
 
 
-    // $('.slider-nav').slick({
-    //   slidesToShow: 3,
-    //   slidesToScroll: 1,
-    //   asNavFor: '.slider-for',
-    //   dots: false,
-    //   arrows: true,
-    //   infinite: true,
-    //   centerMode: true,
-    //   accessibility: false,
-    //   pauseOnHover: true,
-    //   focusOnSelect: true,
-    //   centerPadding: '0px',
-    //   speed: 700,
-    //   draggable: false
-    //   // responsive: [{
-    //   //     breakpoint: 1024,
-    //   //     settings: {
-    //   //       slidesToShow: 5,
-    //   //     }
-    //   //   },
-    //   //   {
-    //   //     breakpoint: 768,
-    //   //     settings: {
-    //   //       slidesToShow: 3,
-    //   //       slidesToScroll: 2
-    //   //     }
-    //   //   }
-    //   // ]
-    // });
+
 
     $('.text-slider').slick({
         dots: true,
@@ -51,68 +23,40 @@ $(document).ready(function () {
         slidesToShow: 1,
         autoplay: true,
         responsive: [
-            // {
-            //     breakpoint: 1600,
-            //     settings: {
-            //         // centerMode: true,
-            //         slidesToShow: 1
-            //     }
-            // },
-            // {
-            //     breakpoint: 1280,
-            //     settings: {
-            //         // centerMode: true,
-            //         slidesToShow: 4
-            //     }
-            // },
-            // {
-            //     breakpoint: 1024,
-            //     settings: {
-            //         // centerMode: true,
-            //         slidesToShow: 3
-            //     }
-            // },
-            // {
-            //     breakpoint: 768,
-            //     settings: {
-            //         // centerMode: true,
-            //         slidesToShow: 2
-            //     }
-            // },
-            // {
-            //     breakpoint: 480,
-            //     settings: {
-            //         // arrows: false,
-            //         // dots: false,
-            //         // centerMode: true,
-            //         // centerPadding: '20px',
-            //         slidesToShow: 1
-            //     }
-            // }
         ]
     });
-
-
-    // $('#adolescence-form').on('submit', function (e) {
-    //     e.preventDefault();
-    //     console.log($(e.currentTarget).serialize());
-    // });
-
-    // $('.modal-language__item').on('click',  function (e) {
-    //     $('.modal-language__item').removeClass('active');
-    //     $(e.currentTarget).addClass('active');
-    // });
 
 
 
     subSlider();
     cloudSlider();
+    mobSlider();
     $(window).on('resize', function() {
         $('.slider-for').slick('unslick');
+        $('.mobile-slider').slick('unslick');
         $('body').attr('data-theme', 1);
         subSlider();
         cloudSlider();
+         mobSlider();
     });
+
+    function mobSlider() {
+        $('.mobile-slider').slick({
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          asNavFor: '.slider-for',
+          dots: false,
+          arrows: false,
+          infinite: true,
+          centerMode: true,
+          accessibility: false,
+          pauseOnHover: true,
+          focusOnSelect: true,
+          centerPadding: '0px',
+          speed: 700,
+          draggable: false
+        });
+    }
 
     function subSlider() {
 
